2c88621b8cd57b567d2a578f87d62202f232233a
Ilia Ulianov <disruptive738@gmail.com>
2024-09-26 15:14:44 +0300

    add project Inline

---
+++ Inline/js/main.js
@@ -0,0 +1,12 @@
+
+document.getElementById('description').onclick = function() {
+    window.location.href = 'description.html';
+  };
+
+  document.getElementById('reviews').onclick = function() {
+    window.location.href = 'reviews.html';
+  };
+
+  document.getElementById('where_from').onclick = function() {
+    window.location.href = 'where_from.html';
+  };
---
+++ Inline/js/slider.js
@@ -0,0 +1,16 @@
+
+
+
+let swiper = new Swiper(".mySwiper", {
+  autoHeight: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
